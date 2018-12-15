2ab1de863c072c78292bb32c46b252b7b93a0065
MistRay <542035625@qq.com>
2018-12-15 23:05:32 +0800

    Initial commit

---
+++ src/main/java/com/mistray/MybatisPlusCustomerApplication.java
@@ -0,0 +1,14 @@
+package com.mistray;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisPlusCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisPlusCustomerApplication.class, args);
+    }
+
+}
+
